e44d988bcf4df6db597a0761495260a603e4f6f8
Zsofi <galambvarizsofia@gmail.com>
2020-11-21 21:18:38 +0100

    add eventlistener and queryselector

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,14 @@
-const modalButton = document.querySelector ('.modalbtn');
-const modal= document.querySelector ('.modal');
-const modalClose= document.querySelector ('.modalClose')
-const close= document.querySelector ('.btn-red')
+var modal = document.querySelector("#modal");
+var modalOverlay= document.querySelector("#modal-overlay");
+var closeButton= document.querySelector("#close-button");
+var openButton= document.querySelector("#open-button");
 
+closeButton.addEventListener("click, function(){
+  modal.classList.toggle("closed);
+  modalOverlay.classList.toggle("closed");
+});
 
+openButton.addEventListener("click", function(){
+  modal.classList.toggle("closed");
+  modalOverlay.classList.toggle("closed");
+});
